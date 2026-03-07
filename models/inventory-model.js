b822536d376e95c69d23831808b073551c1cb520
Rony Reyes <rony@caidci.com>
2026-03-07 11:22:58 -0800

    finishing understanding data results from activity1 week3

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -4,9 +4,9 @@ const pool = require("../database/")
  *  Get all classification data
  * ************************** */
 async function getClassifications(){
-  return await pool.query(
+  const data = await pool.query(
     "SELECT * FROM public.classification ORDER BY classification_name")
-  
+  return data.rows
 }
 
 module.exports = {getClassifications}
